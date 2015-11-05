ee426c5b0d58ab196530d05db9d7d341f3125b06
Taylor Rummel <trummel@mymail.mines.edu>
2015-11-05 12:15:48 -0700

    Implemented Detective Notes and File Menu

--- src/clueGame/ControlGUI.java
+++ src/clueGame/ControlGUI.java
@@ -2,10 +2,15 @@ package clueGame;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
@@ -13,44 +18,77 @@ import javax.swing.border.TitledBorder;
 
 public class ControlGUI extends JFrame {
 	
+	DetectiveGUI detectiveGUI = null;
+	
 	public ControlGUI() {
 		setTitle("Clue Control");
 		setSize(800, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		detectiveGUI = new DetectiveGUI();
+		
+		// Create Menu
+		createFileMenu();
+		
+		// Create Player cards
 		Cards cards = new Cards();
 		add(cards, BorderLayout.EAST);
 		
-		// Create elements of Control Panel
+		// Create and populate Control Panel
 		JPanel controlPanel = new JPanel();
 		controlPanel.setLayout(new GridLayout(2, 3));
 		add(controlPanel, BorderLayout.SOUTH);
 		
 		Header header = new Header();
-		controlPanel.add(header);
 		nextPlayerButton nextPlayer = new nextPlayerButton();
-		controlPanel.add(nextPlayer);
 		makeAccusationButton makeAccusation = new makeAccusationButton();
-		controlPanel.add(makeAccusation);
 		Die die = new Die();
-		controlPanel.add(die);
 		Guess guess = new Guess();
-		controlPanel.add(guess);
 		Result result = new Result();
+		
+		controlPanel.add(header);
+		controlPanel.add(nextPlayer);
+		controlPanel.add(makeAccusation);
+		controlPanel.add(die);
+		controlPanel.add(guess);
 		controlPanel.add(result);
 		
 		
 	}
 	
-	public class ControlPanel extends JPanel {
+	// Creates and populates the File Menu
+	private void createFileMenu() {
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
 		
-		public ControlPanel() {
-			
+		JMenu menu = new JMenu("File");
+		menuBar.add(menu);
+		
+		JMenuItem notes = new JMenuItem("Notes");
+		JMenuItem exit = new JMenuItem("Exit");
+		
+		notes.addActionListener(new MenuNotesListener());
+		exit.addActionListener(new MenuExitListener());
+		
+		menu.add(notes);
+		menu.add(exit);
+	}
+	
+	// Displays Detective Notes
+	public class MenuNotesListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			detectiveGUI.setVisible(true);
+		}
+	}
+	
+	// Exits program
+	public class MenuExitListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
 		}
 	}
 	
 	public class Header extends JPanel {
-		
 		public Header() {
 			// Create layout
 			setLayout(new GridLayout(2, 1));
@@ -69,7 +107,6 @@ public class ControlGUI extends JFrame {
 	}
 	
 	public class nextPlayerButton extends JPanel {
-		
 		public nextPlayerButton() {
 			// Create layout
 			setLayout(new GridLayout(1, 1));
@@ -83,7 +120,6 @@ public class ControlGUI extends JFrame {
 	}
 	
 	public class makeAccusationButton extends JPanel {
-		
 		public makeAccusationButton() {
 			setLayout(new GridLayout(1, 1));
 			
@@ -94,7 +130,6 @@ public class ControlGUI extends JFrame {
 	}
 	
 	public class Die extends JPanel {
-		
 		public Die() {
 			// Create layout
 			setLayout(new GridLayout(1, 2));
@@ -114,7 +149,6 @@ public class ControlGUI extends JFrame {
 	}
 	
 	public class Guess extends JPanel {
-		
 		public Guess() {
 			// Create layout
 			setLayout(new GridLayout(2, 1));
@@ -134,7 +168,6 @@ public class ControlGUI extends JFrame {
 	}
 	
 	public class Result extends JPanel {
-		
 		public Result() {
 			// Create layout
 			setLayout(new GridLayout(1, 2));
@@ -154,7 +187,6 @@ public class ControlGUI extends JFrame {
 	}
 	
 	public class Cards extends JPanel {
-		
 		public Cards() {
 			setLayout(new GridLayout(3, 1));
 			setBorder(new TitledBorder (new EtchedBorder(), "My Cards"));
